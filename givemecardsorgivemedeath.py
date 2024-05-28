52d6f0d8121d1642b4794cb10d14689b7bf15f4f
CarbonIce <80366295+CarbonIce@users.noreply.github.com>
2024-05-28 12:56:12 -0700

    Created combta page and dice data stuctures, working on scraping the website for data

---
+++ givemecardsorgivemedeath.py
@@ -0,0 +1,63 @@
+import requests
+import random as rng
+import time
+from bs4 import BeautifulSoup
+from universalimports import TM, STOP, Dice, CombatPage
+def grabCardsFromPage(page):
+    webpage = requests.get(link + str(page)).text
+
+    soup = BeautifulSoup(webpage, 'html.parser')
+
+    cards = soup.find_all("lor-card")
+    for p in cards:
+        pageTitle = p.find("lor-card-name").find('a').find('span').string
+        pageDiceTable = p.find("table")
+        pageRarity = p['data-rarity']
+        pageDescription = p.find("lor-card-desc").find("span")
+        if pageDescription:
+            onEvent = pageDescription.find("b")
+            if onEvent:
+                action = onEvent.next_sibling.string.strip()
+                onEvent = onEvent.string
+            else:
+                action = None
+        else:
+            onEvent = None
+            action = None
+        dices = pageDiceTable.findAll('tr')
+        initializingPage = CombatPage(pageTitle, pageRarity, onEvent, action, [])
+        for die in dices:
+            dieRange = die.find(class_='range').string
+            dieDesc = die.find(class_='desc')
+            dieType = die['data-detail']
+            dieCounter = die['data-type'] # Counter die are referred to as "Standby" dice
+            if dieCounter == "Standby":
+                dieCounter = True
+            else:
+                dieCounter = False
+            if dieDesc:
+                dieDesc = dieDesc.string
+            dieRange = [int(x) for x in dieRange.split(" - ")]
+            initializingPage.addDie(Dice(dieType, dieRange[0], dieRange[1], dieCounter, dieDesc))
+        print(initializingPage)
+        print("---------------------------------")
+# Look for all lor-card elements
+# Light cost of card is in the element lor-card-icon
+# Card name is under the link element of lor-card-name
+# Dice are under lor-card-back -> lor-card-description
+# Each individual die is in a table under lor-card-description, with range under tr of class range
+link = "https://tiphereth.zasz.su/cards/?dc=1&dc=2&dc=3&dc=4&dc=5&av=Collectable&av=Obtainable&page=" # Add the page number at the end
+page = 1 # Hardcoded so that we get more interesting cards
+rangecount = {}
+while page <= 18:
+    grabCardsFromPage(page)
+    page += 1
+    time.sleep(0.5)
+print('\n'.join([str(key) + ": " + str(rangecount[key]) for key in sorted(rangecount)]))
+possible = 0
+total = 0
+for key in rangecount:
+    if key in [1, 2, 4, 6, 8, 10, 20]:
+        possible += rangecount[key]
+    total += rangecount[key]
+print(f'{possible} / {total}')
---
+++ universalimports.py
@@ -0,0 +1,104 @@
+from random import randint
+class TextModifiers:
+    BLACK = "\033[0;30m"
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    BROWN = "\033[0;33m"
+    BLUE = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    CYAN = "\033[0;36m"
+    LIGHT_GRAY = "\033[0;37m"
+    DARK_GRAY = "\033[1;30m"
+    LIGHT_RED = "\033[1;31m"
+    LIGHT_GREEN = "\033[1;32m"
+    YELLOW = "\033[1;33m"
+    LIGHT_BLUE = "\033[1;34m"
+    LIGHT_PURPLE = "\033[1;35m"
+    LIGHT_CYAN = "\033[1;36m"
+    LIGHT_WHITE = "\033[1;37m"
+    BOLD = "\033[1m"
+    FAINT = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK = "\033[5m"
+    NEGATIVE = "\033[7m"
+    CROSSED = "\033[9m"
+    END = "\033[0m"
+class Dice:
+    def __init__(self, diceType, lowerBound, upperBound, counter=False, description=None):
+        '''
+
+        Dice types
+        Offensive: Slash Pierce Blunt, the damage types
+        Defencive: Block and Evade
+        Counter: Any Offensive or Defensive die can be a Counter die, which is played when facing a one sided attack against 
+        a character that played a page containing a counter die, or has a passive that gives a counter die
+        oh god this entire project is just me nerding out isnt it
+        Additionally, every die could have a variaty of effects, such as On Hit: Inflict 1 Bleed. I'll try to make singular functions
+        for the most common patterns but it's going to be basically impossible.
+
+        '''
+        self.type = diceType
+        self.lowerBound = lowerBound
+        self.upperBound = upperBound
+        self.counter = counter
+        self.description = description
+        self.initial = ""
+        if self.counter: # All counter die are Yellow, regardless of type.
+            self.initial = "C"
+            self.primaryColor = TM.YELLOW
+            self.secondaryColor = TM.YELLOW
+        elif self.type in ["Guard", "Evade"]: # Guard is the term used by the website so thats what I'm going with
+            self.primaryColor = TM.LIGHT_CYAN
+            self.secondaryColor = TM.CYAN
+        else:
+            self.primaryColor = TM.LIGHT_RED
+            self.secondaryColor = TM.RED
+        self.initial += self.type[0]
+    @property
+    def miniStrRepr(self):
+        if self.description:
+            return f"{self.primaryColor}[{self.initial} {self.lowerBound}-{self.upperBound}]{STOP} | {self.description[:min(15, len(self.description))]}..."
+        else:
+            return f"{self.primaryColor}[{self.initial} {self.lowerBound}-{self.upperBound}]{STOP}"
+    def __str__(self):
+        typeModified = self.type
+        if self.counter:
+            typeModified = "Counter " + self.type
+        if self.description:
+            return f"{self.primaryColor}[{typeModified}] {self.secondaryColor}{self.lowerBound} - {self.upperBound}{STOP} | {self.description}"
+        else:
+            return f"{self.primaryColor}[{typeModified}] {self.secondaryColor}{self.lowerBound} - {self.upperBound}{STOP}"
+    def roll(self):
+        return randint(self.lowerBound, self.upperBound)
+class CombatPage:
+    def __init__(self, name, rarity, onEvent, action, dice):
+        self.name = name
+        self.rarity = rarity # Dictates the color of the page name. Nothing else.
+        self.onEvent = onEvent # This is going to be a hellhole. Events are fine, but there are so many custom actions I need to account for.
+        # Prob just going to focus on the ones most critical to gameplay, namely Light Restore and Page Draw
+        self.action = action
+        self.dice = dice
+        match self.rarity:
+            case "Unique": # Object of Art
+                self.color = TM.YELLOW
+            case "Rare": # Limited
+                self.color = TM.BLUE
+            case "Uncommon": # Hardcover
+                self.color = TM.CYAN
+            case "Common": # Paperback
+                self.color = TM.GREEN
+    def popTopDice(self):
+        using = self.dice[0]
+        self.dice = self.dice[1:]
+        return using
+    def addDie(self, die):
+        self.dice.append(die)
+    def __str__(self):
+        # OH GOD WHY
+        return f"{self.color}{self.name}{STOP} | {self.onEvent}: {self.action}\n" + '\n'.join([str(x) for x in self.dice])
+# Constants
+
+# Aliases
+TM = TextModifiers
+STOP = TM.END
